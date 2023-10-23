82b0b8754a376fd901c5d3aca6ac6cff797096c7
Alati11 <peppealati@gmail.com>
2023-10-23 16:36:51 +0200

    add new image

--- js/app.js
+++ js/app.js
@@ -9,13 +9,20 @@ const param = {
 			// qui ci saranno tutte le nostre variabili
 			num: 0,
 			message: 'Ciao sono VUE e sono fighissimo!',
-			className: 'bg-orange text-yellow',
-			inputValue: 'un framework "Magico"',
+			className: 'bgOrange',
+			inputValue: '..."Magico"...',
 			numeri: [0, 5, 6, 9, 8, 4, 5, 8],
+            imgPath:'/Users/giuseppealati/Dev/vue-hello/img/vue.jpeg',
 		}
 	},
+    methods: {
+		getRandomInt(min, max) {
+			console.log('numero random', min, max)
+			return num
+		},
+	},
 }
-
+ 
 const app = createApp(param)
 console.log(app)
 
